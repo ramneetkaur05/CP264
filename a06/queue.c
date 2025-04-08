916b12ee2004f0fd67b346887710dd9479806c82
Ramneet Pannu <158359805+ramneetkaur05@users.noreply.github.com>
2025-04-08 22:52:01 +0000

    insert/delete

---
+++ a06/queue.c
@@ -0,0 +1,40 @@
+/*
+ * file: queue.c 
+ * author: Ramneet Kaur Pannu
+ * date: 8/04/25
+ */ 
+
+ #include <stdio.h>
+ #include "queue.h"
+ 
+ void enqueue(QUEUE *qp, NODE *np) {
+    if (qp->length == 0){
+        qp->front = np;
+        qp->rear = np;
+        qp->front->next = qp->rear->next = NULL;
+    } else {
+        qp->rear->next = np;
+        qp->rear = np;
+        qp->rear->next = NULL;
+    }
+ }  
+ 
+ NODE *dequeue(QUEUE *qp) {
+    NODE *temp = qp->front;
+    if (qp->front == NULL){
+        printf("\nUNDERFLOW!! ABORT! ABORT!");
+        return NULL;
+    } else{
+        qp->front->next = qp->front;
+        qp->length--;
+        return temp;
+
+    }
+ }
+ 
+ void clean_queue(QUEUE *qp) {
+   clean(&(qp->front));
+   qp->front = NULL;
+   qp->rear = NULL;
+   qp->length=0;
+ }
