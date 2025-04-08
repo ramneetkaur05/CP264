6321ed3a3874519a74c9f5557206960c38c498c0
Ramneet Pannu <158359805+ramneetkaur05@users.noreply.github.com>
2025-04-08 19:44:51 +0000

    big int

---
+++ a05/bigint.c
@@ -0,0 +1,61 @@
+/*
+ * file: bigint.h 
+ * author: Ramneet Kaur Pannu
+ * Date: 8/04/25
+ */ 
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include "bigint.h"
+ 
+ BIGINT bigint(char *p) {
+   BIGINT bn = {0};
+   if (p == NULL) 
+     return bn;
+   else if (!(*p >= '0' && *p <= '9')) {// not begin with digits 
+     return bn;
+   }
+   else if (*p == '0' && *(p+1) == '\0') {// just "0"
+     dll_insert_end(&bn, dll_node(*p -'0'));
+     return bn;
+   }  
+   else { 
+     while (*p) {
+       if (*p >= '0' && *p <= '9' ){
+         dll_insert_end(&bn, dll_node(*p -'0'));
+       } else {
+         dll_clean(&bn);
+         break;
+       }
+       p++;
+     }
+     return bn;
+   }
+ }
+ 
+ BIGINT bigint_add(BIGINT op1, BIGINT op2) {
+    BIGINT sum = bigint(NULL);
+
+    NODE *ptr1 = op1.end;
+    NODE *ptr2 = op2.end;
+    int carry = 0, a,b,s;
+
+    while(ptr1 || ptr2){
+        a=0;b=0;
+        if(ptr1){
+            a=ptr1->data;
+            ptr1->prev;
+        }
+        if(ptr2){
+            b=ptr2->data;
+            ptr2->prev;
+        }
+
+    }
+
+
+ }
+ 
+ BIGINT bigint_fibonacci(int n) {
+ // your code
+ }
