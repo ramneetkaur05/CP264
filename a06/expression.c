5135d03df5f2d9bf445550df7e1f81c30c9c2b74
Ramneet Pannu <158359805+ramneetkaur05@users.noreply.github.com>
2025-04-09 02:48:20 +0000

    expression

---
+++ a06/expression.c
@@ -0,0 +1,25 @@
+/*
+ * file: expression.h
+ * author: Ramneet Kaur Pannu
+ * date: 8/04/25
+ */ 
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include "common.h"
+ #include "queue.h"
+ #include "stack.h"
+ #include "expression.h"
+ 
+ QUEUE infix_to_postfix(char *infixstr) {
+ // your code
+ }
+ 
+ int evaluate_postfix(QUEUE queue) {
+ // your code
+ }
+ 
+ int evaluate_infix(char *infixstr) {
+ // your code
+ }
+ 
