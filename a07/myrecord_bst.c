98bb0cb8c4101441bc56b784cff93c54c3b368e5
Ramneet Pannu <158359805+ramneetkaur05@users.noreply.github.com>
2025-04-09 20:19:02 +0000

    myrecord bst finoshed

--- a07/myrecord_bst.c
+++ a07/myrecord_bst.c
@@ -11,12 +11,42 @@
  #include "myrecord_bst.h"
  
  void add_record(BSTDS *ds, RECORD record) {
- // your code
+    bst_insert(&ds->root, record);
+    ds->count++;
+
+    float oldmean = ds->mean;
+    ds->mean = oldmean + (record.score - oldmean) / ds->count;
+    if (ds->count > 1) {
+        ds->stddev = sqrt(((ds->stddev * ds->stddev * (ds->count - 1)) +
+                           (record.score - oldmean) * (record.score - ds->mean)) / ds->count);
+    } else {
+        ds->stddev = 0.0;
+    }
  }
  
  void remove_record(BSTDS *ds, char *name) {
- // your code    
- }
+    BSTNODE *node = bst_search(ds->root, name);
+    if (node){
+        RECORD record = node->data;
+
+        float oldmean = ds->mean;
+        bst_delete(&ds->root, name);
+        ds->count--;
+
+        if (ds->count > 0){
+            ds->mean = oldmean - (record.score - oldmean) / ds->count;
+            if (ds->count > 1){
+                ds->stddev = sqrt(((ds->stddev * ds->stddev * (ds->count - 1)) +
+                (record.score - oldmean) * (record.score - ds->mean)) / ds->count);
+            } else {
+                ds->stddev = 0.0;
+            }
+        } else {
+            ds->mean = 0.0;
+            ds->stddev = 0.0;
+        }
+    }
+  }
  
  void bstds_clean(BSTDS *ds) {
    bst_clean(&ds->root);
