61fcb739cc1923686558dc6d6080c2226cfe1330
Ramneet Pannu <158359805+ramneetkaur05@users.noreply.github.com>
2025-04-10 20:32:13 +0000

    root pointer

--- a08/set_avl.c
+++ a08/set_avl.c
@@ -13,29 +13,29 @@
  }
  
  int set_contain(SET *s, char *e){
-    AVLNODE *root = s->root;
-    return avl_search(root, e)? 1:0;
+    AVLNODE **root = s->root;
+    return avl_search(*root, e)? 1:0;
  }
  
  void set_add(SET *s, char *e){
-    AVLNODE *root = s->root;
+    AVLNODE **root = s->root;
     if (set_contain(s,e) == 0){
         RECORD r = {0};
         strcpy(r.name, e);
-        avl_insert(root, r);
+        avl_insert(*root, r);
         s->size++;
     }
  }
  
  void set_remove(SET *s, char *e){
-    AVLNODE *root = s->root;
+    AVLNODE **root = s->root;
     if (set_contain(s,e) == 0){
-        avl_delete(root, e);
+        avl_delete(*root, e);
         s->size--;
     }
  }
  
  void set_clean(SET *s){
-    AVLNODE *root = s->root;
-    avl_clean(root);
+    AVLNODE **root = s->root;
+    avl_clean(*root);
  }   
