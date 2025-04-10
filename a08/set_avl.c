3ca7f881c0b8bc65f1233825b1b20ed943adb008
Ramneet Pannu <158359805+ramneetkaur05@users.noreply.github.com>
2025-04-10 20:09:25 +0000

    set avl

---
+++ a08/set_avl.c
@@ -0,0 +1,29 @@
+/*
+ * your program signature
+ */ 
+ 
+ #include "string.h" 
+ #include "avl.h"
+ #include "set_avl.h"
+ 
+ int set_size(SET *s) {
+ // your code
+ }
+ 
+ int set_contain(SET *s, char *e){
+ // your code
+ }
+ 
+ void set_add(SET *s, char *e)
+ {
+ // your code
+ }
+ 
+ void set_remove(SET *s, char *e)
+ {
+ // your code    
+ }
+ 
+ void set_clean(SET *s){
+ // your code
+ }   
