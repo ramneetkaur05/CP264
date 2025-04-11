53762e44fa2394dc0a85a854fed7124eba9ff10f
Ramneet Pannu <158359805+ramneetkaur05@users.noreply.github.com>
2025-04-11 20:07:42 +0000

    fixing

--- a09/expression_symbol_ptest.c
+++ a09/expression_symbol_ptest.c
@@ -138,7 +138,7 @@
              }
          }
          printf("enter a statement: e.g. a=(2+3)*2, or all to display hashtable, or quit to exit\n");
-         gets(line);
+         fgetc(line);
          if (strcmp(line, "quit") == 0)
              break;
          
